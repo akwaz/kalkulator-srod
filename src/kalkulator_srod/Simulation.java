8d0e830f65137499bdfc60d3d161ea4ede0e26ef
akwaz <akwaz@users.noreply.github.com>
2016-03-03 22:12:16 +0100

    Już prawie

--- src/kalkulator_srod/Simulation.java
+++ src/kalkulator_srod/Simulation.java
@@ -1,33 +1,37 @@
 package kalkulator_srod;
 
 import java.util.Calendar;
-
 public class Simulation {
 	
-	public Simulation() {
-		Calendar calendar = Calendar.getInstance();
+	private Calendar start, end;
+	
+	public Simulation(Calendar calEnd, Calendar calStart) {
+		start = calStart;//Calendar.getInstance();
+		end = calEnd;
+		//ogólnie to sprawdzam czy dziœ nie jest przypadkiem œroda
+		//je¿eli tak to jeœli jest po 16:00 uznajemy ¿e œroda minê³a
+		//fajne
+		
+		if (start.get(Calendar.DAY_OF_WEEK) == Calendar.WEDNESDAY && start.get(Calendar.HOUR_OF_DAY) >= 16)
+			start.add(Calendar.DAY_OF_MONTH, 1);
+	
+		
+		if (start.compareTo(end) > 0) {
+			
+		}
 	}
 
 	public int howManyWeds() {
 		int numOfWeds = 0;
-	
+		
+		while(start.compareTo(end) < 0){
+			if (start.get(Calendar.DAY_OF_WEEK) == Calendar.WEDNESDAY){
+				numOfWeds++;
+			}
+			start.add(Calendar.DAY_OF_MONTH, 1);
+		}
+		
 		return numOfWeds;
 	}
-	  // Safety pig has arrived!
-	  //                               _
-	  //  _._ _..._ .-',     _.._(`))
-	  // '-. `     '  /-._.-'    ',/
-	  //    )         \            '.
-	  //   / _    _    |             \
-	  //  |  a    a    /              |
-	  //  \   .-.                     ;  
-	  //   '-('' ).-'       ,'       ;
-	  //      '-;           |      .'
-	  //         \           \    /
-	  //         | 7  .__  _.-\   \
-	  //         | |  |  ``/  /`  /
-	  //        /,_|  |   /,_/   /
-	  //           /,_/      '`-'
-	  //
 
 }
--- src/kalkulator_srod/Window.java
+++ src/kalkulator_srod/Window.java
@@ -6,14 +6,18 @@ import javax.swing.JFrame;
 import org.eclipse.wb.swing.FocusTraversalOnArray;
 import java.awt.Component;
 import javax.swing.JLabel;
+import javax.swing.JOptionPane;
+
 import java.awt.Font;
 import javax.swing.SwingConstants;
 import java.awt.SystemColor;
+import java.util.Calendar;
 
 public class Window {
 
 	private JFrame frame;
-
+	private Calendar start, end;
+	
 	/**
 	 * Launch the application.
 	 */
@@ -29,23 +33,21 @@ public class Window {
 			}
 		});
 	}
-
-	/**
-	 * Create the application.
-	 */
 	public Window() {
 		initialize();
 	}
 
-	/**
-	 * Initialize the contents of the frame.
-	 */
 	private void initialize() {
+		
+
+		//wygenerowane automatycznie
+		//obowi¹zkowe œmieszne komentarze
 		frame = new JFrame();
-		frame.setBounds(100, 100, 221, 301);
+		frame.setBounds(100, 100, 220, 300);
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		frame.getContentPane().setLayout(null);
 		frame.setResizable(false);
+		frame.setLocationRelativeTo(null);
 		JLabel lblDoKocaRoku = new JLabel("Do ko\u0144ca roku jeszcze");
 		lblDoKocaRoku.setFont(new Font("Open Sans", Font.PLAIN, 18));
 		lblDoKocaRoku.setBounds(10, 11, 195, 48);
@@ -55,16 +57,57 @@ public class Window {
 		labelNumOfWed.setForeground(SystemColor.textHighlight);
 	    labelNumOfWed.setVerticalAlignment(SwingConstants.CENTER);
 		labelNumOfWed.setFont(new Font("Open Sans", Font.PLAIN, 150));
-		labelNumOfWed.setBounds(0, 59, 205, 127);
+		labelNumOfWed.setBounds(0, 59, 214, 127);
 		
 		frame.getContentPane().add(labelNumOfWed);
 		
-		JLabel labelWed = new JLabel("New label");
-		labelWed.setBounds(73, 217, 46, 14);
+		JLabel labelWed = new JLabel("", SwingConstants.CENTER);
+		labelWed.setFont(new Font("Open Sans", Font.PLAIN, 22));
+		labelWed.setBounds(10, 197, 195, 45);
 		frame.getContentPane().add(labelWed);
 		
+		//co za spaghetti!
+		//nie bój nic, na szczêœcie œwinka bezpieczeñstwa przyby³a!
+		//                               _
+		//  _._ _..._ .-',     _.._(`))
+		// '-. `     '  /-._.-'    ',/
+		//    )         \            '.
+		//   / _    _    |             \
+		//  |  a    a    /              |
+		//  \   .-.                     ;  
+		//   '-('' ).-'       ,'       ;
+		//      '-;           |      .'
+		//         \           \    /
+		//         | 7  .__  _.-\   \
+		//         | |  |  ``/  /`  /
+		//        /,_|  |   /,_/   /
+		//           /,_/      '`-'
+
+		start = Calendar.getInstance();
+
+		end = Calendar.getInstance();
+		end.set(Calendar.YEAR, 2016);
+		end.set(Calendar.MONTH, Calendar.JUNE);
+		end.set(Calendar.DAY_OF_MONTH, 24);
+
+		if (start.compareTo(end) > 0) {
+			JOptionPane.showMessageDialog(null, "Data wa¿noœci programu minê³a.", "To koniec!", JOptionPane.ERROR_MESSAGE);
+			System.exit(0);
+		}
+		Simulation sim = new Simulation(end, start);
 		
-		Simulation sim = new Simulation();
+		int numOfWeds = sim.howManyWeds();
 		
+		if (numOfWeds == 1)
+			labelWed.setText("œroda");
+		else if (numOfWeds == 0)
+			labelWed.setText("œród");
+		else if (numOfWeds >= 2 && numOfWeds <=4)
+			labelWed.setText("œrody");
+		else if (numOfWeds >= 5 && numOfWeds <= 21)
+			labelWed.setText("œród");
+
+		
+		labelNumOfWed.setText(Integer.toString(numOfWeds));
 	}
 }
